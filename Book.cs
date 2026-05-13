58a236ae88f826fb1c3ead33d249fa02364748cf
ShoroukShawky <324243558@sha.edu.eg>
2026-05-13 03:17:37 +0300

    ..

---
+++ Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF01
+{
+    internal class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int NumberOfPages { get; set; }
+
+        public int Year { get; set; }
+    }
+}
---
+++ BookStoreContext .cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF01
+{
+    internal class BookStoreContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-35R1TT2\\SQLEXPRESS;Database=EFCore01;Trusted_Connection=True;TrustServerCertificate = true");
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Categorie> Categories { get; set; }
+
+    }
+}
